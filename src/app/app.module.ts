801555edaaedfb0143d7553d587cee9d83165900
Brent Marquez <brentonmarquez@gmail.com>
2019-09-15 14:43:52 -0600

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersService } from "./servers/servers.service";
+
+// import Routes type from @angular/router
+/**
+ * The routes is an array of objects - each object must have a path key set to a string which is what goes in the URL
+ * after the domain.
+ *   Note: Do not add the slash / in the path
+ * Each object has a component key set to a component to load for the path
+ */
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "users", component: UsersComponent },
+  { path: "users/:id", component: UserComponent },
+  { path: "servers", component: ServersComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
